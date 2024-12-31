97c7a9639df94433cd8772f5db81cf746e7a81c7
adinho97 <adrian.dys97@gmail.com>
2024-12-31 15:27:59 +0100

    Delete UIManager

--- obj/Debug/net8.0/GameDevProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameDevProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameDevProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0179505456c2fc4adfa7f85bf0c6cd4ca196c911")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30b618213260ae6ddd8eeb25bce91e1e2d49e462")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameDevProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameDevProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
